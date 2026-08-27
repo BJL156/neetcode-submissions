8a42ad64c739d5900b8094c36356e2066853cf30
Brayden Leone <97370242+BJL156@users.noreply.github.com>
2026-08-27 16:42:10 -0400

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int collectedWater = 0;
+
+        int left = 0;
+        int right = height.size() - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    collectedWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    collectedWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return collectedWater;
+    }
+};
